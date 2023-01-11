774bfc5a88b5b78fdd098ce3502a9f553cd93158
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-11 18:01:24 +0330

    Type : object

--- build/script.js
+++ build/script.js
@@ -1,28 +1,15 @@
 "use strict";
-function sum(x, y) {
-    return x + y;
-}
-console.log(sum(5, 6));
-let sum2 = function (x, y) {
-    return x + y;
-};
-console.log(sum2(5, 'amir'));
-let sum3 = (x, y) => x + y;
-console.log(sum3(5, 'amir'));
-let test;
-test = function (a, b) {
-    return 'amirm + MJ';
-};
-console.log(test('', ''));
-let sum5 = (x = 1, y) => {
-    if (y) {
-        return x + y;
+let person = {
+    name: 'amirreza',
+    age: (num) => {
+        return `age: ${num}`;
     }
-    return x;
 };
-console.log(sum5(3, 4));
-console.log(sum5());
-let sum6 = (x = 1, ...numbers) => {
-    console.log(x, numbers);
+let person2 = {
+    name: 'marzieh',
+    age: (num) => {
+        return `age: ${num}`;
+    }
 };
-console.log(sum6(2, 22, 33));
+console.log(person);
+console.log(person2);
--- src/script.ts
+++ src/script.ts
@@ -1,48 +1,28 @@
-function sum(x: number, y: number): number {
-    return x + y;
+type MyObject = {
+name : string ,
+age : (num : number) => string
 }
 
 
-console.log(sum(5, 6));
 
-
-let sum2 = function (x: any, y: any): string {
-    return x + y;
+let person : MyObject =
+{
+    name : 'amirreza',
+    age : (num : number)=>{
+       return `age: ${num}`
+    } 
 }
 
 
-console.log(sum2(5, 'amir'));
 
-let sum3 = (x: any, y: any): string => x + y;
-
-
-console.log(sum3(5, 'amir'));
-
-
-let test: (a: string, b: string) => any;
-test = function (a: string, b: string) {
-    return 'amirm + MJ'
+let person2 :MyObject =
+{
+    name : 'marzieh',
+    age : (num : number)=>{
+       return `age: ${num}`
+    } 
 }
 
 
-console.log(test('', ''));
-
-
-
-let sum5 = (x:number = 1 , y?:number )=> {
-    if(y){
-        return x+y;
-    }
-    return x;
-}
-
-
-console.log(sum5(3,4));
-console.log(sum5());
-
-
-let sum6 = (x:number = 1 , ...numbers : number[] )=> {
- console.log(x , numbers)
-}
-
-console.log(sum6(2,22,33))
+console.log(person)
+console.log(person2)
