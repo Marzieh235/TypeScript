718ca6287745f662b323fdf89a7ccfd1a70840be
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-12 02:03:06 +0330

    interface as class types

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,47 @@
-interface SearchFunc {
-    (source :  string , subString : string) : boolean
+interface Person {
+    name : string
+    family : string
+    age : number
+    fullname() : string
 }
 
 
-let mySearch : SearchFunc = (source : string , subString : string) => {
-    let result = source.search(subString);
-    return result > -1;
+class Alex implements Person {
+    constructor(public name : string , public family : string , public age : number) {
+    }
+
+    fullname() : string {
+        return this.name + ' ' + this.family;
+    }
+}
+
+class Hesam implements Person {
+    constructor(public name : string , public family : string , public age : number) {
+    }
+
+    fullname() : string {
+        return this.name + ' ' + this.family;
+    }
+}
+
+class Ali implements Person {
+    constructor(public name : string , public family : string , public age : number) {
+    }
+
+    fullname() : string {
+        return this.name + ' ' + this.family;
+    }
+}
+
+
+let user : Person = new Hesam('hesam' , 'family' , 26)
+user = new Alex('hesam' , 'family' , 26)
+
+
+console.log(user.fullname())
+
+class Auth {
+    login(user : Person) {
+        // user.password
+    }
 }
