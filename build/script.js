0ddf06e3da66e7cc1de137f9d8f4424b05d67dae
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-12 01:07:00 +0330

    static & readOnly

--- build/script.js
+++ build/script.js
@@ -1,19 +1,16 @@
 "use strict";
 class Car {
     constructor(model, year) {
-        this.model = model;
-        this.year = year;
-        this.model = model;
+        Car.model = model;
         this.year = year;
     }
-    foo() {
-        console.log(this.bar());
-    }
 }
-class text extends Car {
-    bar() {
-        return ('hi');
-    }
-}
-let car1 = new text('410', 2012);
-console.log(car1.bar());
+// class text extends Car {
+//     bar() {
+//      return ('hi')
+//     }
+// }
+let car1 = new Car('410', 2012);
+console.log(Car.model);
+console.log(car1.year);
+// console.log(car1.bar())
--- src/script.ts
+++ src/script.ts
@@ -1,27 +1,28 @@
-abstract class Car {
-    constructor(protected model: string, public year: number) {
-        this.model = model
+ class Car {
+    static model : string
+    readonly year : number
+    constructor(model : string, year : number) {
+        Car.model = model
         this.year = year
     }
 
 
-    abstract bar(): any;
-    foo () :void {
-        console.log (this.bar())
-    }
+//     abstract bar(): any;
+//     foo () :void {
+//         console.log (this.bar())
+//     }
 }
 
-    class text extends Car {
-        bar() {
-         return ('hi')
+    // class text extends Car {
+    //     bar() {
+    //      return ('hi')
 
-        }
+    //     }
 
-    }
-
-
+    // }
 
 
-
-let car1 = new text('410', 2012)
-console.log(car1.bar())
+let car1 = new Car('410', 2012)
+console.log(Car.model)
+console.log(car1.year)
+// console.log(car1.bar())
