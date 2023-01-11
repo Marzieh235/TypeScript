65c949c97f9bb366d6f6ba91af9e6c62424bf764
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-11 15:51:28 +0330

    undefined , null & void Types

--- build/script.js
+++ build/script.js
@@ -1,7 +1,15 @@
 "use strict";
 let fullname = 'amirreza';
-let num = true;
-let virtual = 5;
-let list = [1, true, 3];
-list = [];
-let tuple = ['amir', 0];
+fullname = getstring();
+function getstring() {
+    return null;
+}
+function log(message) {
+    console.log(message);
+}
+log('hello my love');
+// let num: boolean = true;
+// let virtual : number = 5
+// let list: any = [1,true,3]
+// list = []
+// let tuple :[string , number] = ['amir' , 0]
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,27 @@
-let fullname: string = 'amirreza';
-let num: boolean = true;
-let virtual : number = 5
+let fullname: string | null = 'amirreza';
+
+fullname = getstring()
+
+
+function getstring(){
+    return null ;
+}
+
+
+function log (message :string ): void{
+    console.log(message)
+}
+
+log('hello my love')
+
+// let num: boolean = true;
+// let virtual : number = 5
 
 
 
-let list: any = [1,true,3]
-list = []
+
+// let list: any = [1,true,3]
+// list = []
 
 
-let tuple :[string , number] = ['amir' , 0]
+// let tuple :[string , number] = ['amir' , 0]
