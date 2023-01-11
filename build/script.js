6e436de22cbc26fe526e129de4c2df65ca4bd314
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-11 22:20:15 +0330

    Encapsulation in TSC

--- build/script.js
+++ build/script.js
@@ -3,7 +3,19 @@ class Car {
     constructor(model, year) {
         this.model = model;
         this.year = year;
+        this.model = model;
+        this.year = year;
+    }
+    sayHello() {
+        console.log('hello everyOne');
     }
 }
+class Irankhodro extends Car {
+    run() {
+        return (this.sayHello);
+    }
+}
+let test = new Irankhodro('405', 1400);
 let car1 = new Car('206', 1380);
-console.log(car1);
+console.log(car1.year);
+console.log(test.run());
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,25 @@
 class Car {
-    model : string 
-    year : number
-    constructor (model : string , year : number){
-        this.model = model 
+    constructor(private model: string, public year: number) {
+        this.model = model
         this.year = year
+    }
+
+
+    protected sayHello() {
+        console.log('hello everyOne')
+    }
+
+}
+
+class Irankhodro extends Car {
+    run() {
+       return(this.sayHello)
     }
 }
 
-let car1 = new Car ('206' , 1380)
-console.log(car1)
+
+let test = new Irankhodro('405' ,1400)
+let car1 = new Car('206', 1380)
+console.log(car1.year)
+console.log(test.run())
+
