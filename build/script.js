637d760b4b0afa69d74cee4a2cb32c1ee57c814f
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-12 02:53:41 +0330

    generics in classes

--- build/script.js
+++ build/script.js
@@ -1,9 +1,20 @@
 "use strict";
-function logData(data) {
-    return data;
+class MyArray {
+    constructor(data, data2) {
+        this.data = data;
+        this.data2 = data2;
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    getItem(index) {
+        return this.data[index];
+    }
 }
-let myLogFunc;
-myLogFunc = function (data) {
-    return data;
-};
-console.log(myLogFunc);
+let list = new MyArray(['iitem1 ', 'item2'], [false, true]);
+list.addItem('4');
+console.log(list.getItem(0));
+function logParameters(x, y) {
+    console.log(x, y);
+}
+logParameters('2', true);
--- src/script.ts
+++ src/script.ts
@@ -1,17 +1,25 @@
-function logData<T>(data : T) : T {
-    return data;
+class MyArray<T extends number | string,U> {
+    constructor(public data : T[] , private data2 : U[]) {}
+
+    addItem(item : T) {
+        this.data.push(item)
+    }
+
+    getItem(index : number) {
+        return this.data[index]
+    }
 }
 
-interface myLogFuncInterface<T> {
-    (data : T) : T
+
+let list = new MyArray<string , boolean>(['iitem1 ', 'item2'] , [false ,true])
+list.addItem('4')
+console.log(list.getItem(0))
+
+
+
+function logParameters<T extends number | string,U extends boolean | object>(x : T , y : U) : void {
+    console.log(x , y)
 }
 
-let myLogFunc : myLogFuncInterface<number> ;
-myLogFunc = function(data : number ) : number {
-    return data
-}
 
-
-
-
-console.log(myLogFunc)
+logParameters<string , boolean>('2' , true)
